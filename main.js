397d4b05404e6eb57c906e2e2e89b060b4f661a9
baianacarai <138497780+baianacarai@users.noreply.github.com>
2025-05-22 11:48:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -35,19 +35,18 @@ function calculaTempo(tempoObjetivo){
     minutos %= 60;
     horas %= 24;
     if (tempoFinal > 0){
-        return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+        return [dias,horas,minutos,segundos];
     } else {
         return [0,0,0,0];
     }
 }
 
 function atualizaCronometro(){
-    document.getElementsById("dias0").textContent = calculaTempo(tempos[0])[0]
-    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1]
-    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2]
-    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3]
     for (let i=0; i<contadores.length;i++){
-       // contadores[i].textContent = calculaTempo(tempos[i])
+    document.getElementsById("dias"+i).textContent = calculaTempo(tempos[i])[0];
+    document.getElementById("horas"+i).textContent = calculaTempo(tempos[i])[1];
+    document.getElementById("min"+i).textContent = calculaTempo(tempos[i])[2];
+    document.getElementById("seg"+i).textContent = calculaTempo(tempos[i])[3];
     }
 }
 
@@ -56,4 +55,4 @@ function comecaCronometro(){
     setInterval(atualizaCronometro,1000);
 }
 
-comecaCronometro()
+comecaCronometro();
